50ab86d2791d2a7f27f695d1b867a340bfb10e99
torekhan <torekhan93@mail.ru>
2017-11-06 15:26:09 +0000

    Testing

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,4 +1,5 @@
 import { AppPage } from './app.po';
+import { browser } from 'protractor';
 
 describe('angular App', () => {
   let page: AppPage;
@@ -7,8 +8,33 @@ describe('angular App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+  it('should display welcome message Ristorante Con Fusion', () => {
+    page.navigateTo('/');
+    expect(page.getParagraphText('app-root h1')).toEqual('Ristorante Con Fusion');
   });
+
+  it('should navigate to aboutus page by clicking on the link', () => {
+    page.navigateTo('/');
+
+    const navlink = page.getAllElements('a').get(1);
+    navlink.click();
+
+    expect(page.getParagraphText('h1')).toBe('About us');
+  });
+
+  it('should enter a new comment for the first dish', () => {
+    page.navigateTo('/dishdetail/0');
+
+    const newAuthor = page.getElement('input[type=text]');
+    newAuthor.sendKeys('Test Author');
+
+    const newComment = page.getElement('textarea');
+    newComment.sendKeys('Test Comment');
+
+    const newSubmitButton = page.getElement('button[type=submit]');
+    newSubmitButton.click();
+
+    browser.pause();
+  });
+
 });
--- src/app/menu/menu.component.spec.ts
+++ src/app/menu/menu.component.spec.ts
@@ -1,16 +1,48 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { MaterialModule } from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { MenuComponent } from './menu.component';
+
+import { Dish } from '../shared/dish';
+import { DishService } from '../services/dish.service';
+import { DISHES } from '../shared/dishes';
+import { baseURL } from '../shared/baseurl';
+import { Observable } from 'rxjs/Observable';
+
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
 
 describe('MenuComponent', () => {
   let component: MenuComponent;
   let fixture: ComponentFixture<MenuComponent>;
 
   beforeEach(async(() => {
+
+    const dishServiceStub = {
+      getDishes: function(): Observable<Dish[]> {
+        return Observable.of(DISHES);
+      }
+    };
+
     TestBed.configureTestingModule({
-      declarations: [ MenuComponent ]
+      imports: [ BrowserAnimationsModule,
+        MaterialModule,
+        FlexLayoutModule,
+        RouterTestingModule.withRoutes([{ path: 'menu', component: MenuComponent }])
+      ],
+      declarations: [ MenuComponent ],
+      providers: [
+        { provide: DishService, useValue: dishServiceStub },
+        { provide: 'BaseURL', useValue: baseURL },
+      ]
     })
     .compileComponents();
+
+    const dishservice = TestBed.get(DishService);
+
   }));
 
   beforeEach(() => {
@@ -19,7 +51,26 @@ describe('MenuComponent', () => {
     fixture.detectChanges();
   });
 
+
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('dishes items should be 4', () => {
+    expect(component.dishes.length).toBe(4);
+    expect(component.dishes[1].name).toBe('Zucchipakoda');
+    expect(component.dishes[3].featured).toBeFalsy();
+  });
+
+  it('should use dishes in the template', () => {
+    fixture.detectChanges();
+
+    let de:      DebugElement;
+    let el:      HTMLElement;
+    de = fixture.debugElement.query(By.css('h1'));
+    el = de.nativeElement;
+
+    expect(el.textContent).toContain(DISHES[0].name.toUpperCase());
+
   });
 });
